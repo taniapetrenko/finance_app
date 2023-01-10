3f3aa1f2091fb7648d5efe75a58b6f13f40c408a
Tania Petrenko <tanyuwka96@ukr.net>
2023-01-10 20:27:27 +0200

    Create Operation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_10_181955) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_10_182612) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -18,4 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_10_181955) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "operations", force: :cascade do |t|
+    t.decimal "amount"
+    t.datetime "odate"
+    t.string "description"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_operations_on_category_id"
+  end
+
+  add_foreign_key "operations", "categories"
 end
