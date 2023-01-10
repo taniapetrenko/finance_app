98d55f6cda1536fed7102de57455ee360111e7d1
Tania Petrenko <tanyuwka96@ukr.net>
2023-01-10 21:16:37 +0200

    Add validations

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,7 @@
 class Operation < ApplicationRecord
   belongs_to :category
+
+  validates :amount, presence: true,
+                     numericality: { only_integer: true, greater_than: 0 }
+  validates :odate, :description, presence: true
 end
